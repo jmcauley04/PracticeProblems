f80ec2a838340ca81f755af1cce8f539ac3af0b8
Michael Cauley <jmcauley04@gmail.com>
2022-04-23 06:59:16 -0400

    P1 completed

--- PracticeProblemsTests/Sorting/P1_SortingLinkedListTests.cs
+++ PracticeProblemsTests/Sorting/P1_SortingLinkedListTests.cs
@@ -12,7 +12,7 @@ namespace PracticeProblems.Sorting.Tests
         [DataRow(0, 1)]
         [DataRow(1, 1)]
         [DataRow(3, 3, 3, 13, 1, 3, 7)]
-        public void MergeSortTest_1(int nodeASize, params int[] ints)
+        public void MergeSortTests(int nodeASize, params int[] ints)
         {
             P1_SortingLinkedList.Node? a = null;
             P1_SortingLinkedList.Node? b = null;
@@ -24,6 +24,32 @@ namespace PracticeProblems.Sorting.Tests
                 b = new P1_SortingLinkedList.Node(ints[nodeASize..]);
 
             var result = P1_SortingLinkedList.MergeSort(a, b);
+
+            foreach (var i in ints.OrderBy(x => x))
+            {
+                Assert.AreEqual(i, result!.Data);
+                result = result.Next;
+            }
+        }
+
+        [TestMethod()]
+        [DataRow(3, 3, 10, 20, 1, 5, 7)]
+        [DataRow(0)]
+        [DataRow(0, 1)]
+        [DataRow(1, 1)]
+        [DataRow(3, 3, 3, 13, 1, 3, 7)]
+        public void SolutionTests(int nodeASize, params int[] ints)
+        {
+            P1_SortingLinkedList.Node? a = null;
+            P1_SortingLinkedList.Node? b = null;
+
+            if (nodeASize > 0)
+                a = new P1_SortingLinkedList.Node(ints[0..nodeASize]);
+
+            if (ints.Length - nodeASize > 0)
+                b = new P1_SortingLinkedList.Node(ints[nodeASize..]);
+
+            var result = P1_SortingLinkedList.Solution(a, b);
 
             foreach (var i in ints.OrderBy(x => x))
             {
